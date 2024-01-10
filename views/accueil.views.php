d16fba770163833a6703d86ee1370b78616aea29
Aurélien Bois <cci@aurelienbois.fr>
2024-01-10 09:39:40 +0100

    Corrected file names

--- views/accueil.views.php
+++
@@ -1,7 +0,0 @@
-<?php ob_start(); // start c'est comme une banane dans le pot d'échappement de PHP
-?>
-<h1>Index (accueil)</h1>
-<?php
-$title = 'Accueil - site de YY';
-$content = ob_get_clean(); // clean c'est comme retirer la banane du pot d'échappement
-require_once 'template.php';

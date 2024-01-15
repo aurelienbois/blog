5b176a880191a69596f35a1680045b67cc32c016
Aurélien Bois <cci@aurelienbois.fr>
2024-01-15 20:32:25 +0100

    Blog post layout and image display

--- views/blog.view.php
+++ views/blog.view.php
@@ -5,30 +5,34 @@ require_once ('models/Post.class.php');
 ob_start(); // start c'est comme une banane dans le pot d'échappement de PHP
 ?>
 <h1>Blog</h1>
+<div class="row">
 <?php  foreach ($posts as $p) { ?>
-<div class="card mb-3">
-    <a href="./blog/lire/<?= $p->getId() ?>" class="post text-body text-decoration-none">
-        <h3 class="card-header"><?= $p->getHeader() ?></h3>
-        <div class="card-body">
-            <h5 class="card-title"><?= $p->getTitle() ?></h5>
-            <h6 class="card-subtitle text-muted"><?= $p->getAuthor() ?></h6>
+    <div class="col-md-6">
+        <div class="card mb-3">
+            <a href="./blog/lire/<?= $p->getId() ?>" class="post text-body text-decoration-none">
+                <h3 class="card-header"><?= $p->getHeader() ?></h3>
+                <div class="card-body">
+                    <h5 class="card-title"><?= $p->getTitle() ?></h5>
+                    <h6 class="card-subtitle text-muted"><?= $p->getAuthor() ?></h6>
+                </div>
+                <img src="<?= $p->getImage() ?>" alt="..." class="img-fluid" style="width: 100%">
+                <div class="card-body">
+                    <p class="card-text"><?= nl2br($p->getContent()) ?></p>
+                </div>
+                <div class="card-footer text-muted">
+                    <?php
+                        echo $p->getDate(true);
+                    ?>
+                </div>
+            </a>
+            <div class="card-body">
+                <a href="#" class="card-link">Modifier</a>
+                <a href="#" class="card-link">Supprimer</a>
+            </div>
         </div>
-        <img src="<?= $p->getImage() ?>" alt="...">
-        <div class="card-body">
-            <p class="card-text"><?= nl2br($p->getContent()) ?></p>
-        </div>
-        <div class="card-footer text-muted">
-            <?php
-                echo $p->getDate(true);
-            ?>
-        </div>
-    </a>
-    <div class="card-body">
-        <a href="#" class="card-link">Modifier</a>
-        <a href="#" class="card-link">Supprimer</a>
     </div>
-</div>
 <?php } ?>
+</div>
 <a href="/blog/add"><button type="button" class="btn btn-secondary">Ajouter un post</button></a>
 <?php
 $title = 'Blog';
